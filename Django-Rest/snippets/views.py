5d47c95d07a29594c280af6c9c48d6756a331ef8
Lucas Rijllart <lucas@yobota.xyz>
2018-03-29 12:13:31 +0100

    Fixed UserViewSet
    Fixed an import to the correct snippets.serializers.UserSerializer

--- Django-Rest/snippets/views.py
+++ Django-Rest/snippets/views.py
@@ -11,6 +11,7 @@ from rest_framework.reverse import reverse
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
+from snippets.serializers import UserSerializer
 
 class SnippetViewSet(viewsets.ModelViewSet):
     """
